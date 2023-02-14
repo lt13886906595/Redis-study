bb8d29fbeae67f5854e05f47fe64f413b9fed695
longteng <1185232995@qq.com>
2023-02-14 15:20:22 +0800

    redis-demo

---
+++ src/main/java/com/heima/redis/config/Test1.java
@@ -0,0 +1,14 @@
+package com.heima.redis.config;
+
+import lombok.Data;
+
+/**
+ * @className: Test1
+ * @description: 类描述
+ * @author: lt
+ * @date: 2023/2/10
+ **/
+@Data
+public class Test1 {
+    private String name;
+}
